e061ac5b1e1608b18043bec12c67b7da80594db2
Paulius <paulius75@hotmail.com>
2017-04-06 21:37:01 +0300

    3paskaita_Uzduotis_1_2

---
+++ src/paskaita3/Uzduotis_1_2.java
@@ -0,0 +1,23 @@
+package paskaita3;
+
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 2017.04.06.
+ */
+public class Uzduotis_1_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String zodis;
+        do {
+            System.out.println("Iveskite zodi ");
+            zodis = sc.next();
+            if (zodis.equals("pabaiga")){
+                System.out.println("aciu uz varga");
+            }else{
+                System.out.println(zodis);
+                System.out.println("-----------");
+            }
+        }while (zodis.equals("pabaiga")==false);
+    }
+}
