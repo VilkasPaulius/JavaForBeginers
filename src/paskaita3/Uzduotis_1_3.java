5df08028c8ef92e5207b59c4b4e362d60b184a16
Paulius <paulius75@hotmail.com>
2017-04-06 21:48:28 +0300

    3paskaita_Uzduotis_1_3

---
+++ src/paskaita3/Uzduotis_1_3.java
@@ -0,0 +1,20 @@
+package paskaita3;
+
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 2017.04.06.
+ */
+public class Uzduotis_1_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int suma =0;
+        int sk;
+        do{
+            System.out.println("Iveskite skaiciu");
+            sk = sc.nextInt();
+            suma += sk;
+        }while (sk!=0);
+        System.out.println("Ivestu skaiciu suma: "+suma);
+    }
+}
