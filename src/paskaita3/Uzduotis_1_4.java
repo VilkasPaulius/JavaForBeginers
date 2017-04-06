defc7fe6dc2c3e286c3f0d2b5c72a2e69a7946fe
Paulius <paulius75@hotmail.com>
2017-04-07 00:07:55 +0300

    3paskaita_Uzduotis_1_4

---
+++ src/paskaita3/Uzduotis_1_4.java
@@ -0,0 +1,27 @@
+package paskaita3;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 2017.04.06.
+ */
+public class Uzduotis_1_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int suma =0;
+        int sk = 0;
+        do{
+            try {
+                System.out.println("Iveskite skaiciu");
+                sk = sc.nextInt();
+                suma += sk; //prie sumos pridedam naujai ivesta skaiciu
+            }catch (InputMismatchException a){
+                System.out.println("Ivedete ne skaiciu");
+                continue;
+            }
+        }while (sk!=0);
+        System.out.println("Ivestu skaiciu suma: "+suma);
+    }
+}
