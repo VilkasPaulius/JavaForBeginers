f6446e462f94eb57c285beedc26ab457aeefa848
Paulius <paulius75@hotmail.com>
2017-04-09 01:05:05 +0300

    3paskaita_Uzduotis_1_16

---
+++ src/paskaita3/Uzduotis_1_15.java
@@ -0,0 +1,40 @@
+package paskaita3;
+
+
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 2017.04.07.
+ */
+public class Uzduotis_1_15 {
+    public static void main(String[] args) {
+
+        raides();
+    }
+
+
+    private static void raides (){
+        Scanner sc = new Scanner(System.in);
+        String zodis;
+        char raide;
+        System.out.println("Iveskite zodi");
+        zodis = sc.next();
+        System.out.println();
+        System.out.println("iveskite raide");
+        raide = sc.next().charAt(0);
+        int ilg =  zodis.length();
+        //System.out.println(ilg);
+        int kiek = 0;
+
+        //System.out.println("zodzio raide"+ zodis.charAt(1));
+        for (int i=0; i<zodis.length();i++){
+            if (raide==zodis.charAt(i)){
+                kiek++;
+            }//ar reikia else?
+        }
+        System.out.println("Zodyje "+zodis+" raidziu "+raide+" yra: "+kiek);
+
+
+
+    }
+}
